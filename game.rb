8817ff75729eded1223e9070288633f82e1fbdc9
Tzheng456 <Tzheng456@gmail.com>
2021-09-14 18:34:52 -0700

    abstract player initializations into game.rb, implement game_over? to check if player1 or player2 life is 0, assign winner accordingly, implement next_turn, increments turn, and rotates the current player, implement display_score to print out a current score table, implement play which calls question, answer, display_score, next_turn until game_over? then outputs a winner message

--- game.rb
+++ game.rb
@@ -1,39 +1,88 @@
+require_relative './player'
+
 class Game
-  attr_accessor :player1, :player2, :winner
+  attr_accessor :players, :winner, :turn
 
   def initialize
-    @player1 = nil
-    @player2 = nil
+    @players = []
     @winner = nil
   end
 
-  def gameStart(p1, p2)
-    @player1 = p1
-    @player2 = p2
-    @currentPlayer = player1
+  def game_start
+
+    puts "Player 1: (enter a name)"
+    p1_name = gets.chomp
+    puts "Player 2: (enter a name)"
+    p2_name = gets.chomp
+
+    p1 = Player.new(p1_name)
+    p2 = Player.new(p2_name)
+    @players = [p1, p2]
+    @currentPlayer = players[0]
+    @turn = 1
+
     puts "----------MATH QUIZ GAME----------"
-    puts "#{player1.name} VS #{player2.name}"
+    puts "         #{players[0].name} VS #{players[1].name}"
     puts "----------------------------------"
+    puts
   end
 
   def question
     num1 = rand(1...100)
     num2 = rand(1...100)
     @sum = num1 + num2
-    puts "Question for #{@currentPlayer.name}!"
+    puts "-------Turn ##{@turn}: #{@currentPlayer.name}-------"
     puts "What is the sum of #{num1} + #{num2} equal to?"
   end
 
   def answer
     ans = gets.chomp.to_i
+    puts
     if ans == @sum
       puts "CORRECT ANSWER! #{@currentPlayer.name}, you answered #{ans}."
-      puts "#{@currentPlayer.name} has #{@currentPlayer.life} life remaining."
     else
       puts "WRONG ANSWER! #{@currentPlayer.name} LOSES 1 LIFE!"
       @currentPlayer.life -= 1
-      puts "#{@currentPlayer.name} has #{@currentPlayer.life} life remaining."
     end
+    puts
+  end
+
+  def game_over?
+    if @players[0].life == 0
+      @winner = @players[1].name
+      return true
+    end
+    if @players[1].life == 0
+      @winner = @players[0].name
+      return true
+    end
+  end
+
+  def next_turn
+    @turn += 1
+    @players.rotate!
+    @currentPlayer = @players[0]
+  end
+
+  def display_score
+    puts "----------CURRENT SCORE----------"
+    puts "#{@players[0].name} Life: #{@players[0].life}/3"
+    puts "#{@players[1].name} Life: #{@players[1].life}/3"
+    puts "---------------------------------"
+  end
+
+  def play
+    until(game_over?) do
+      question
+      answer
+      display_score
+      next_turn
+    end 
+
+    puts "--------------------"
+    puts "  Winner: #{@winner}!"
+    puts "--------------------"
+
   end
   
 end
--- main.rb
+++ main.rb
@@ -1,17 +1,9 @@
 require './player'
 require './game'
 
-puts "Player 1: (enter a name)"
-p1_name = gets.chomp
-puts "Player 2: (enter a name)"
-p2_name = gets.chomp
 
-p1 = Player.new(p1_name)
-p2 = Player.new(p2_name)
 
 match = Game.new
 
-match.gameStart(p1, p2)
-
-match.question
-match.answer
+match.game_start
+match.play
